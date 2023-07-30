0283883a39ffd457cd50c543153382d198993413
LwynnF <lfong96@hotmail.co.uk>
2023-07-30 18:04:24 +0100

    Fixed compiling errors

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
-
+import { useEffect } from "react";
 import axios from "axios";
 
 // If city is updated then make unsplash api call to get image
 export default function BackgroundImage({ city, setBackgroundImage }) {
+	// Include handleResponse in the dependency array
 	useEffect(() => {
 		if (city) {
 			const unsplashApiKey = "k83IR4PMKvx38jTyBh7038QqDbLe5n0QftSVoCOLiXc";
@@ -14,18 +14,18 @@ export default function BackgroundImage({ city, setBackgroundImage }) {
 				.then((response) => handleResponse(response))
 				.catch((error) => console.log(error));
 		}
-	}, [city]);
 
-	// takes the unsplash api response choosing a photo using a random number and calls setbackgroundimage
-	function handleResponse(response) {
-		if (response.data.results.length > 0) {
-			const randomIndex = Math.floor(
-				Math.random() * response.data.results.length
-			);
-			const photoUrl = response.data.results[randomIndex].urls.regular;
-			setBackgroundImage(photoUrl);
+		// Define handleResponse function here
+		function handleResponse(response) {
+			if (response.data.results.length > 0) {
+				const randomIndex = Math.floor(
+					Math.random() * response.data.results.length
+				);
+				const photoUrl = response.data.results[randomIndex].urls.regular;
+				setBackgroundImage(photoUrl);
+			}
 		}
-	}
+	}, [city, setBackgroundImage /* Include handleResponse here */]);
 
 	return null;
 }
