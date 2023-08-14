4db492ca9b44609192902c16626bc763c4308d40
LwynnF <lfong96@hotmail.co.uk>
2023-08-14 11:20:36 +0100

    Fixed forecast displaying before search button clicked

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,12 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
-import "./Search.css";
+import "./Search.css"
 
-export default function Search({ onCityChange }) {
+export default function Weather({ onCityChange }) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
 	const [city, setCity] = useState("Edinburgh");
-
-	const search = useCallback((city) => {
-		const apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
-		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-		axios
-			.get(apiUrl)
-			.then((response) => {
-				handleResponse(response);
-			})
-			.catch((error) => {
-				console.error("API Error:", error);
-			});
-	}, []);
-
-	useEffect(() => {
-		// Fetch weather data for the default city when the component mounts
-		search(city);
-	}, [search, city]);
 
 	function handleResponse(response) {
 		setWeatherData({
@@ -45,44 +26,51 @@ export default function Search({ onCityChange }) {
 	function handleSubmit(event) {
 		event.preventDefault();
 		onCityChange(city);
-
-		// Call the search function to fetch weather data
-		search(city);
+		const apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 
-	function handleCityChange(event) {
-		setCity(event.target.value);
-	}
+	useEffect(() => {
+		handleSubmit({ preventDefault: () => {} });
+	}, []); // Call API on initial render
 
-	return (
-		<div className="Search">
-			<form onSubmit={handleSubmit}>
-				<div className="row">
-					<div className="col-8">
-						<input
-							type="search"
-							className="form-control"
-							autoFocus={false}
-							placeholder="Enter a city"
-		
-							onChange={handleCityChange}
-						/>
-					</div>
-					<div className="col-4">
-						<input
-							type="submit"
-							value="Search"
-							className="btn btn-light w-100"
-						/>
-					</div>
-				</div>
-			</form>
-			{weatherData.ready && (
-				<>
-					<WeatherInfo data={weatherData} />
-					<WeatherForecast coordinates={weatherData.coordinates} />
-				</>
-			)}
-		</div>
-	);
+
+	  function handleCityChange(event) {
+			setCity(event.target.value);
+		}
+
+
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-8">
+            <input
+              type="search"
+              className="form-control"
+              autoFocus={false}
+              placeholder="Enter a city"
+              onChange={handleCityChange}
+            />
+          </div>
+          <div className="col-4">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-light w-100"
+            />
+          </div>
+        </div>
+      </form>
+      {weatherData.ready && (
+        <>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+        </>
+      )}
+    </div>
+  );
 }
+
+
