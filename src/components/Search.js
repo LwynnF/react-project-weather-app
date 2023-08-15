3b57712cbf4e4af8a1bbfc93a88642b4cb9d5df9
LwynnF <lfong96@hotmail.co.uk>
2023-08-15 10:35:18 +0100

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
-import "./Search.css"
+import "./Search.css";
 
 export default function Weather({ onCityChange }) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
@@ -31,46 +31,40 @@ export default function Weather({ onCityChange }) {
 		axios.get(apiUrl).then(handleResponse);
 	}
 
-	useEffect(() => {
-		handleSubmit({ preventDefault: () => {} });
-	}, []); // Call API on initial render
+	useEffect(() => {}, []); // Call API on initial render
 
+	function handleCityChange(event) {
+		setCity(event.target.value);
+	}
 
-	  function handleCityChange(event) {
-			setCity(event.target.value);
-		}
-
-
-  return (
-    <div className="Search">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-8">
-            <input
-              type="search"
-              className="form-control"
-              autoFocus={false}
-              placeholder="Enter a city"
-              onChange={handleCityChange}
-            />
-          </div>
-          <div className="col-4">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-light w-100"
-            />
-          </div>
-        </div>
-      </form>
-      {weatherData.ready && (
-        <>
-          <WeatherInfo data={weatherData} />
-          <WeatherForecast coordinates={weatherData.coordinates} />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="Search">
+			<form onSubmit={handleSubmit}>
+				<div className="row">
+					<div className="col-8">
+						<input
+							type="search"
+							className="form-control"
+							autoFocus={false}
+							placeholder="Enter a city"
+							onChange={handleCityChange}
+						/>
+					</div>
+					<div className="col-4">
+						<input
+							type="submit"
+							value="Search"
+							className="btn btn-light w-100"
+						/>
+					</div>
+				</div>
+			</form>
+			{weatherData.ready && (
+				<>
+					<WeatherInfo data={weatherData} />
+					<WeatherForecast coordinates={weatherData.coordinates} />
+				</>
+			)}
+		</div>
+	);
 }
-
-
